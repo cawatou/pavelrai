d3b096aeab6cb2c3d828e75f9b25d8e5e1a8e149
cawatou <cawatou@gmail.com>
2018-02-28 20:52:34 +0300

    service (p1)

---
+++ wp-content/themes/NativeChurch/template-service.php
@@ -0,0 +1,147 @@
+<?php
+/*
+	Template Name: service
+ */
+get_header(); ?>
+<div class="page_content">
+	<div class="container_cont service">
+		<article>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="/vse-uslugi/stoimost-pohoron/" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service1.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Стоимость похорон</h5>
+							<div class="meta-data">
+								
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service2.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Заказать кремацию</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service3.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Вызов агента</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service4.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Груз 200</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service5.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Катафалк</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service6.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Уход за могилкой</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service4.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Бронзовые буквы</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service5.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Эпитафии</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="box_staff col-sm-4">
+				<div class="grid-item staff-item">
+					<a href="#" class="grid-item-inner">
+						<div class="media-box">
+							<img src="/wp-content/themes/NativeChurch/images/service/service6.png" alt="">
+						</div>
+						<div class="grid-content">
+							<h5>Индивидуальный заказ памятника</h5>
+							<div class="meta-data">
+
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+<?php get_footer(); ?>
