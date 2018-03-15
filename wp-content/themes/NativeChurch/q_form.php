4e93ef7a59284c147dfe2688261949468ead9ef7
cawatou <cawatou@gmail.com>
2018-03-15 21:26:50 +0300

    contact page

--- wp-content/themes/NativeChurch/q_form.php
+++ wp-content/themes/NativeChurch/q_form.php
@@ -1,16 +1,16 @@
 <form id="q_form" >
-        <h2 class="text-center">Остались вопросы</h2>
-        <p class="text-center">Оставьте заявку на консультацию</p>
-        <p class="text-center">Наш специалист свяжется с вами и ответит на все вопросы</p>
-        <div class="col-md-6">
-            <div class="form-group">
-                <input type="text" class="form-control" id="q_name" aria-describedby="emailHelp" placeholder="Введите ваше имя">
-            </div>
+    <h2 class="text-center">Остались вопросы</h2>
+    <p class="text-center">Оставьте заявку на консультацию</p>
+    <p class="text-center">Наш специалист свяжется с вами и ответит на все вопросы</p>
+    <div class="col-md-6">
+        <div class="form-group">
+            <input type="text" class="form-control" id="q_name" aria-describedby="emailHelp" placeholder="Введите ваше имя">
         </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <input type="text" class="form-control" id="q_phone" aria-describedby="emailHelp" placeholder="Введите номер телефона">
-            </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <input type="text" class="form-control" id="q_phone" aria-describedby="emailHelp" placeholder="Введите номер телефона">
         </div>
-        <button type="submit" class="btn btn-primary center-block">Задать вопрос</button>
-    </form>
+    </div>
+    <button type="submit" class="btn btn-primary center-block">Задать вопрос</button>
+</form>
--- wp-content/themes/NativeChurch/template-contact.php
+++ wp-content/themes/NativeChurch/template-contact.php
@@ -5,10 +5,37 @@
 get_header();
 $custom = get_post_custom(get_the_ID()); // карта 
 ?>
-<div class="page_content">
+<div class="page_content contact_page">
 	<div id="content_contacts" class="container_content">
-		<?php the_content(); ?>
+		<div class="row">
+			<div class="col-md-4 col_tel">
+				<p><strong>Телефоны:</strong></p>
+				<p>+7 914 711-28-20</p>
+				<p>+7 4234 32-14-33</p>
+				<p>+7 4234 37-28-20</p>
+				<p>+7 4234 32-16-94</p>
+			</div>
+			<div class="col-md-3 col_mail">
+				<p><strong>Почта:</strong></p>
+				<p>7pavel7@mail.ru</p>
+			</div>
+			<div class="col-md-5 col_form">
+				<span><strong>Не нашли ответа на свой вопрос?</strong></span>
+				<p>Спросите наших специалистов</p>
+				<p>и мы обязательно вам ответим</p>
+				<button type="submit" class="btn btn-primary center-block">Задать вопрос</button>
+			</div>
+		</div>
+
+		<div class="adress">
+			<p><strong>Наш адрес:</strong></p>
+			<p>692502, Приморский край, г.Уссурийск</p>
+			<p>ул. Некрасова, 22 офис 219, второй этаж</p>
+			<p>(здание треста 34)</p>
+		</div>
+
 		<div id="contacts_map" class="post-content">
+			<p><strong>Как нас найти:</strong></p>
 			<?php
 				if ($custom['imic_contact_map_display'][0] == 'yes' && !empty($custom['imic_contact_map_box_code'][0])) {
 					echo '<div id="gmap">';
@@ -17,9 +44,11 @@ $custom = get_post_custom(get_the_ID()); // карта
 				}
 			?>
 			<div id="gmap2">
-				<p>База предприятия</p>
+				<p><strong>База предприятия</strong></p>
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2880.972759499308!2d132.0007306625726!3d43.77342429164205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5fb2e8a3109959e3%3A0x329bd80ac902f259!2z0YPQuy4g0KjRgtCw0LHRgdC60L7Qs9C-LCAxMCwg0KPRgdGB0YPRgNC40LnRgdC6LCDQn9GA0LjQvNC-0YDRgdC60LjQuSDQutGA0LDQuSwgNjkyNTAy!5e0!3m2!1sru!2sru!4v1415699743591" width="750" height="350" frameborder="0" style="border:0"></iframe></iframe>
 			</div>
+			
+			<?if(0 == 1):?>
 			<div class="row">
 				<form method="post" id="contactform" name="contactform" class="contact-form" action="<?php echo get_template_directory_uri() ?>/mail/contact.php">
 					<div class="col-md-6 margin-15">
@@ -48,7 +77,9 @@ $custom = get_post_custom(get_the_ID()); // карта
 				<div class="clearfix"></div>
 				<div id="message"></div>
 			</div>
+			<?endif;?>
 		</div>
+		
 	</div>
 </div>
 <?php get_footer(); ?>
