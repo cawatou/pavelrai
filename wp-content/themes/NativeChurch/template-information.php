53191f5af7cb501453ff3a83963088ef00ec7819
cawatou <cawatou@gmail.com>
2018-03-15 20:01:11 +0300

    information page

--- wp-content/themes/NativeChurch/template-information.php
+++ wp-content/themes/NativeChurch/template-information.php
@@ -4,64 +4,27 @@
  */
 get_header();
 ?>
-<div class="page_content">
-	<div class="container_content information_page">
-		<ul>
-            <li>
-                <a href="">
-                    <p class="date">18.01.2018</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">16.01.2018</p>
-                    <p class="title">Изготовление и установка памятника</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">04.01.2018</p>
-                    <p class="title">Выбор и оплата памятника</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">15.12.2017</p>
-                    <p class="title">Изготовление оградок и их установка</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">18.11.2017</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">4.08.2017</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">04.01.2018</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">15.12.2017</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <p class="date">18.11.2017</p>
-                    <p class="title">Действия в случае смерти человека</p>
-                </a>
-            </li>
-        </ul>
-	</div>	
-</div>
-<?php get_footer(); ?>
+    <div class="page_content">
+        <div class="container_content information_page">
+            <?php
+            $args = array(
+                'post_parent' => 10386,
+                'post_type'   => 'any',
+                'numberposts' => -1,
+                'post_status' => 'any'
+            );
+            $children = get_children( $args );?>
+            <ul>
+                <?foreach ($children as $child):?>
+                    <li>
+                        <a href="<?=$child->post_name?>">
+                            <p class="date"><?=$child->post_date?></p>
+                            <p class="title"><?=$child->post_title?></p>
+                        </a>
+                    </li>
+                <?endforeach;?>
+            </ul>
+            <?=q_form()?>
+        </div>
+    </div>
+<?php get_footer();?>
