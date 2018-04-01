d7c56659963d30e00c4eb0c4bda01969fb362871
ljkh <cawatou@gmail.com>
2018-04-02 02:15:36 +0300

    install, service - edit

--- wp-content/themes/NativeChurch/template-install-type.php
+++ wp-content/themes/NativeChurch/template-install-type.php
@@ -5,49 +5,78 @@
 get_header();
 ?>
 <div class="page_content install_type_content">
-	<div class="container_cont">
-		<div class="install_type col-md-4">
-			<a href="?page_id=813">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i1.png" alt=""><br/>
-				<p class="text-center">Установка</p>
-				<p class="text-center">в металлическую рамку</p>
-			</a>
-		</div>
-		<div class="install_type col-md-4">
-			<a href="?page_id=825">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i2.png" alt=""><br/>
-				<p class="type_title">Установка на плиту</p>
-				<p class="type_title">"Подиум"</p>
-			</a>
-		</div>
-		<div class="install_type col-md-4">
-			<a href="?page_id=825">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i3.png" alt=""><br/>
-				<p class="type_title">Установка на плиту </p>
-				<p class="type_title">«Миниракушка» </p>
-			</a>
-		</div>
-		<div class="install_type col-md-4">
-			<a href="?page_id=838">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i4.png" alt=""><br/>
-				<p class="type_title">Установка на бетонный</p>
-				<p class="type_title">фундамент «Стандарт»</p>
-			</a>
-		</div>	
-		<div class="install_type col-md-4">
-			<a href="?page_id=825">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i5.png" alt=""><br/>
-				<p class="type_title">Установка на плиту</p>
-				<p class="type_title">«Ракушка»</p>
-			</a>
-		</div>
-		<div class="install_type col-md-4">
-			<a href="?page_id=825">
-				<img src="<?php bloginfo('template_url'); ?>/images/install/i6.png" alt=""><br/>
-				<p class="type_title">Установка на плиту</p>
-				<p class="type_title">«Греция»</p>
-			</a>
-		</div>
+	<div class="container_cont product-archive mainpage-archive mock_catalog">
+        <ul class="products">
+            <li class="product">
+                <a href="?page_id=813">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i1.png" alt="">
+                   </div>
+                    <div class="wrapper_attr">
+                        <p class="text-center">Установка</p>
+                        <p class="text-center">в металлическую рамку</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/shop/combined/9749061/">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i2.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Установка на плиту</p>
+                        <p class="type_title">"Подиум"</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="?page_id=825">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i3.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Установка на плиту </p>
+                        <p class="type_title">«Миниракушка» </p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="?page_id=838">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i4.png" alt=""><br/>
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Установка на бетонный</p>
+                        <p class="type_title">фундамент «Стандарт»</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="?page_id=825">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i5.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Установка на плиту</p>
+                        <p class="type_title">«Ракушка»</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="?page_id=825">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/install/i6.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Установка на плиту</p>
+                        <p class="type_title">«Греция»</p>
+                    </div>
+                </a>
+            </li>
+
+        </ul>
+
+
 	</div>	
 </div>
 <?php get_footer(); ?>
--- wp-content/themes/NativeChurch/template-service.php
+++ wp-content/themes/NativeChurch/template-service.php
@@ -4,144 +4,100 @@
  */
 get_header(); ?>
 <div class="page_content">
-	<div class="container_cont service">
-		<article>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/stoimost-pohoron/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service1.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Стоимость похорон</h5>
-							<div class="meta-data">
-								
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/zakazat-krematsiyu/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service2.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Заказать кремацию</h5>
-							<div class="meta-data">
+	<div class="container_cont service product-archive mainpage-archive mock_catalog">
+        <ul class="products">
+            <li class="product">
+                <a href="/vse-uslugi/stoimost-pohoron/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service1.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="text-center">Стоимость похорон</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/vse-uslugi/zakazat-krematsiyu/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service2.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Заказать кремацию</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="/vse-uslugi/vyzov-agenta/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service3.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Вызов агента</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/vse-uslugi/gruz-200/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service4.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Груз 200</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/vse-uslugi/katafalk/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service5.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Катафалк</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="/vse-uslugi/uhod-za-mogilkoj/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service6.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Уход за могилкой</p>
+                    </div>
+                </a>
+            </li>
 
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/vyzov-agenta/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service3.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Вызов агента</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/gruz-200/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service4.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Груз 200</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/katafalk/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service5.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Катафалк</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/uhod-za-mogilkoj/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service6.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Уход за могилкой</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/bronzovye-bukvy/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service4.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Бронзовые буквы</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/epitafii/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service5.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Эпитафии</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-			<div class="box_staff col-sm-4">
-				<div class="grid-item staff-item">
-					<a href="/vse-uslugi/individualnyj-zakaz-pamyatnika/" class="grid-item-inner">
-						<div class="media-box">
-							<img src="/wp-content/themes/NativeChurch/images/service/service6.png" alt="">
-						</div>
-						<div class="grid-content">
-							<h5>Индивидуальный заказ памятника</h5>
-							<div class="meta-data">
-
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-		</article>
+            <li class="product">
+                <a href="/vse-uslugi/bronzovye-bukvy/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service7.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Бронзовые буквы</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/vse-uslugi/epitafii/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service8.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Эпитафии</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="/vse-uslugi/individualnyj-zakaz-pamyatnika/">
+                    <div class="wraper_img">
+                        <img src="/wp-content/themes/NativeChurch/images/service/service9.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Индивидуальный заказ памятника</p>
+                    </div>
+                </a>
+            </li>
+        </ul>
 	</div>
 </div>
 <?//echo do_shortcode( '[Best_Wordpress_Gallery id="33" gal_title="На главной"]' );?>
