392c0f62b84c7e47bf61db053315ae247fcd5ea7
ljkh <cawatou@gmail.com>
2018-04-02 20:14:58 +0300

    scandir carousel

--- wp-content/themes/NativeChurch/works_carousel.php
+++ wp-content/themes/NativeChurch/works_carousel.php
@@ -1,41 +1,18 @@
+<?$dir = $_SERVER['DOCUMENT_ROOT'].'/wp-content/uploads/photo-gallery/main';
+$files = scandir($dir);
+?>
+
 <div id="our_works" class="carousel_block">
     <p class="title">Наши работы</p>
     <div class="owl-carousel ">
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-1.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-1.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-2.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-2.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-3.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-3.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-4.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-4.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-5.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-5.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-6.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-6.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-7.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-7.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-8.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-8.jpg" alt="">
-        </a>
-
-        <a href="/wp-content/uploads/photo-gallery/main/portfolio-9.jpg" class="zoom" data-rel="prettyPhoto[product-gallery]">
-            <img src="/wp-content/uploads/photo-gallery/main/portfolio-9.jpg" alt="">
-        </a>
+        <?foreach($files as $img):
+            $check = explode('.', $img);
+            if(count($check) == 2 && $check[0] != ''):?>
+                <a href="/wp-content/uploads/photo-gallery/main/<?=$img?>" class="zoom" data-rel="prettyPhoto[product-gallery]">
+                    <img src="/wp-content/uploads/photo-gallery/main/<?=$img?>" alt="">
+                </a>
+            <?endif?>
+        <?endforeach?>
     </div>
     <img class='left_arr' src="/wp-content/themes/NativeChurch/images/left_arr.png" />
     <img class='right_arr' src="/wp-content/themes/NativeChurch/images/right_arr.png" />
