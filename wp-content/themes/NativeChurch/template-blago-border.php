feac31e62ead5e29972086eb5706fcdf121042d7
ljkh <cawatou@gmail.com>
2018-04-05 02:42:17 +0300

    gallery_work (p1)

--- wp-content/themes/NativeChurch/template-blago-border.php
+++ wp-content/themes/NativeChurch/template-blago-border.php
@@ -4,24 +4,27 @@
  */
 get_header();
 ?>
-<div class="page_content">
-	<div class="container_content blago-border_page">
-        <?php the_content() ?>
-        <div class="col-md-6">
-            <p>Было</p>
-            <img src="/wp-content/uploads/2014/11/New-Project-321.png">
-        </div>
+    <div class="page_content">
+        <div class="container_content blago-border_page">
+            <?php the_content() ?>
+            <div class="col-md-6">
+                <p><strong>Было</strong></p>
+                <img src="/wp-content/uploads/2014/11/New-Project-321.png">
+            </div>
 
-        <div class="col-md-6">
-            <p>Стало</p>
-            <img src="/wp-content/uploads/2014/11/New-Project-311.png">
-        </div>     
-        <div>
-            <p>Ниже указаны примеры наших работ по установке бордюров:</p>
+            <div class="col-md-6">
+                <p><strong>Стало</strong></p>
+                <img src="/wp-content/uploads/2014/11/New-Project-311.png">
+            </div>
+            <div>
+                <br>
+                <p>Ниже указаны примеры наших работ по установке бордюров:</p>
+            </div>
+            <div class="gallery_work">
+                <p class="title">Наши работы</p>
+                <?=do_shortcode( '[Best_Wordpress_Gallery id="45" gal_title="Бордюры и щебень (Благоустройство)"]' )?>
+            </div>
+            <?=q_form()?>
         </div>
-           
-        <?//=do_shortcode( '[Best_Wordpress_Gallery id="36" gal_title="Бордюры и щебень (Благоустройство)"]' )?>
-        <?=q_form()?>
-	</div>	
-</div>
+    </div>
 <?php get_footer(); ?>
---
+++ wp-content/themes/NativeChurch/template-blago.php
@@ -0,0 +1,76 @@
+<?php
+/*
+  Template Name: blago
+ */
+get_header();
+?>
+<div class="page_content install_type_content">
+	<div class="container_cont product-archive mainpage-archive mock_catalog">
+        <ul class="products">
+            <li class="product">
+                <a href="/shop-category/care/macadam">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago1.png" alt="">
+                   </div>
+                    <div class="wrapper_attr">
+                        <p class="text-center">Щебень и мраморная крошка</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/blagoustroistvo/bordyury-i-shheben/">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago2.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Бордюры и щебень</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="/blagoustroistvo/bruschatka/">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago3.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Брусчатка</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/shop-category/care/platform">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago4.png" alt=""><br/>
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Фигурные плиты и площадки</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product">
+                <a href="/blagoustroistvo/fundamenty/">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago5.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Фундаменты</p>
+                    </div>
+                </a>
+            </li>
+            <li class="product last">
+                <a href="/shop-category/care/monument">
+                    <div class="wraper_img">
+                        <img src="<?php bloginfo('template_url'); ?>/images/blago6.png" alt="">
+                    </div>
+                    <div class="wrapper_attr">
+                        <p class="type_title">Гранитные монументы</p>
+                    </div>
+                </a>
+            </li>
+
+        </ul>
+
+
+	</div>	
+</div>
+<?php get_footer(); ?>
